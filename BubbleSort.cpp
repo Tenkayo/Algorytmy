1bab316ac98b44895ea7d35da7968e85c81dca01
Tenkayo <joachimvonvolkenrose@gmail.com>
2023-05-29 15:49:14 +0200

    add .gitignore file and quicksort file, also add statistic-generating functions to bubblesort

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <time.h>
 
 using namespace std;
 
@@ -48,3 +50,92 @@ int BubbleSort2(int * tab, int length)
     return comparisonsTotal;
 }
 
+int* RandomTable(int size)
+{
+    if (size<1) return NULL;    
+    int* Table = new int[size];
+    for (int i = 0; i < size; i++) Table[i] = rand();
+    return Table;
+}
+
+void WriteData(int* Table, int size)
+{
+    if (Table == NULL || size <1) cout << "Data error!" << endl;
+    else for (int i = 0; i < size; i++) cout << Table[i] << " ";        
+}
+
+int LosujIndeks(int length)
+{
+    int i;
+    do i = rand(); while (i >= length);
+    return i;
+}
+
+float BubbleSortStatistics1(int max, int length)
+{
+    srand(time(NULL));
+    int *PointerToTable, SessionComparisons;
+    float TotalComparisons = 0;
+    for (int i = 0; i < max; i++)
+    {
+        PointerToTable = RandomTable(length);
+        SessionComparisons = BubbleSort1(PointerToTable, length);
+        cout << "Dla tablic o dlugosci " << length << " wykonano " << i+1 << " petli" << endl;
+        cout << "Wartosc SessionComparisons: " << SessionComparisons << endl;
+        TotalComparisons+=SessionComparisons;
+        cout << "Wartosc TotalComparisons " << TotalComparisons << endl;
+    }
+    return TotalComparisons/max;
+}
+
+float BubbleSortStatistics2(int max, int length)
+{
+    srand(time(NULL));
+    int *PointerToTable, SessionComparisons;
+    float TotalComparisons = 0;
+    for (int i = 0; i < max; i++)
+    {
+        PointerToTable = RandomTable(length);
+        SessionComparisons = BubbleSort2(PointerToTable, length);
+        cout << "Dla tablic o dlugosci " << length << " wykonano " << i+1 << " petli" << endl;
+        cout << "Wartosc SessionComparisons: " << SessionComparisons << endl;
+        TotalComparisons+=SessionComparisons;
+        cout << "Wartosc TotalComparisons " << TotalComparisons << endl;
+    }
+    return TotalComparisons/max;
+}
+
+void TestBubbleSort1()
+{
+    ofstream DataFile;
+    DataFile.open("bubblesort1.dat");
+    float results;
+    for (int i = 10; i <= 1000; i+=10)
+    {
+        results = BubbleSortStatistics1(100,i);
+        DataFile << i << "   " << results << endl; 
+        cout << "Progress: " << i << "/1000" << endl;
+    }
+    DataFile.close();
+}
+
+void TestBubbleSort2()
+{
+    ofstream DataFile;
+    DataFile.open("bubblesort2.dat");
+    float results;
+    for (int i = 10; i <= 1000; i+=10)
+    {
+        results = BubbleSortStatistics2(100,i);
+        DataFile << i << "   " << results << endl; 
+        cout << "Progress: " << i << "/1000" << endl;
+    }
+    DataFile.close();
+}
+
+int main()
+{
+    TestBubbleSort1();
+    TestBubbleSort2();
+    return 0;
+}
---
+++ Quicksort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <time.h>
+#include <fstream>
+
+using namespace std;
+
+void swap(int * array, int index1, int index2)
+{
+    int temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
+}
+
+int quicksort(int* tab, int left, int right)
+{
+    if (left >= right) return 0;
+    int pos = left;
+    for (int i = pos+1; i > right; i++) if(tab[i]<tab[left]) swap(tab,++pos,i);
+    swap(tab,pos,left);
+    return quicksort(tab,left,pos-1) + quicksort(tab,pos+1,right);
+}
